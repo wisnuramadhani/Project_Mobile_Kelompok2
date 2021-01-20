c70c1a65510bfe5ea2bcc6df65c5a41cdf593a66
wisnuramadhani <52442195+wisnuramadhani@users.noreply.github.com>
2021-01-20 07:58:29 +0700

    20-01-2021

---
+++ app/src/main/java/com/example/project_mobile_kelompok2/tes.kt
@@ -0,0 +1,11 @@
+package com.example.project_mobile_kelompok2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tes)
+    }
+}
